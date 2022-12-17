2f87b42d499c97fc833e065b7fecb5d489def09b
RadoStef <rs_perfect@mail.bg>
2022-12-17 15:30:48 +0200

    welcome and facts sections added

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const nextButton = document.getElementById('next-btn');
 const questionContainerEl = document.getElementById('question-container');
 const questionEl = document.getElementById('question');
 const answerButtonsEl = document.getElementById('answer-buttons');
+const welcomeEl = document.getElementById('welcome');
+const factsEl = document.getElementById('answer-facts');
 
 startButton.addEventListener('click', startGame);
+welcomeEl.classList.remove('hide');
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion();
-})
+});
 
 function startGame() {
     console.log(`game started`);
@@ -19,13 +22,14 @@ function startGame() {
     shuffledQuestions = questions.sort(() => Math.random() - .5 )
     currentQuestionIndex = 0;
     questionContainerEl.classList.remove('hide');
+    welcomeEl.classList.add('hide');
     setNextQuestion();
-}
+};
 
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+};
 
 function showQuestion(question) {
     questionEl.innerText = question.question
@@ -45,8 +49,8 @@ function resetState() {
     nextButton.classList.add('hide')
     while (answerButtonsEl.firstChild) {
         answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-    }
-}
+    };
+};
 
 function selectAnswer(e) {
     const selectedButton = e.target
@@ -76,34 +80,79 @@ function setStatusClass(element, correct) {
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
-}
+};
 
 const questions = [
     {
-        question: `What is 2 + 2`,
+        question: `What country has the most natural lakes?`,
         answers: [
-            { text: '4', correct: true },
-            { text: '22', correct: false},
-            { text: '2.2', correct: false},
-            { text: '0.2', correct: false}
+            { text: 'Canada', correct: true },
+            { text: 'USA', correct: false},
+            { text: 'Brasil', correct: false},
+            { text: 'Finlandia', correct: false}
         ] 
     },
     {
-        question: `What is 4 - 4`,
+        question: `Which Central American country has a name which translates to English as “The Saviour”?`,
         answers: [
-            { text: '8', correct: false },
-            { text: '4', correct: false },
-            { text: '0', correct: true },
-            { text: 'NaN', correct: false },
+            { text: 'Panama', correct: false },
+            { text: 'Guatemala', correct: false },
+            { text: 'Costa Rica', correct: false },
+            { text: 'El Salvador', correct: true },
         ] 
     },
     {
-        question: `The capital of Bulgaria is:`,
+        question: `What's the city with the most diversity in terms of language?`,
         answers: [ 
-            { text: 'Sofia', correct: true },
-            { text: 'Praha', correct: false },
-            { text: 'Rousse', correct: false },
-            { text: 'Yambol', correct: false },
+            { text: 'Chicago', correct: false },
+            { text: 'Los Angeles', correct: false },
+            { text: 'London', correct: false },
+            { text: 'New York', correct: true },
+        ]
+    },
+    {
+        question: `Which US state is Westernmost but at the same time - Easternmost?`,
+        answers: [ 
+            { text: 'California', correct: false },
+            { text: 'Washington', correct: false },
+            { text: 'Alaska', correct: true },
+            { text: 'Nebraska', correct: false },
+        ]
+    },
+    {
+        question: `How many time zones Russia has?`,
+        answers: [ 
+            { text: '12', correct: false },
+            { text: '7', correct: false },
+            { text: '9', correct: false },
+            { text: '11', correct: true },
+        ]
+    },
+    {
+        question: `How many islands the Phillipines has?`,
+        answers: [ 
+            { text: '7100', correct: true },
+            { text: '6350', correct: false },
+            { text: '85', correct: false },
+            { text: '12003', correct: false },
+        ]
+    },
+    {
+        question: ``,
+        answers: [ 
+            { text: 'Chicago', correct: false },
+            { text: 'Los Angeles', correct: false },
+            { text: 'London', correct: false },
+            { text: 'New York', correct: false },
+        ]
+    },
+    {
+        question: `The oldest continuously inhabited city in the world is?`,
+        answers: [ 
+            { text: 'Damascus', correct: false },
+            { text: 'Kairo', correct: false },
+            { text: 'Baghdad', correct: false },
+            { text: 'Paris', correct: false },
         ]
     }
 ];
